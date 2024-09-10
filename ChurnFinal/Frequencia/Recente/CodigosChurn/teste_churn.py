4334a9dd5d469c9336c58cdd5bd5a3518bf3dd84
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2024-09-09 22:40:12 -0300

    Organizado

---
+++ ChurnFinal/Frequencia/Recente/CodigosChurn/teste_churn.py
@@ -0,0 +1,6 @@
+import Churn as c 
+
+e = c.e
+
+teste = c.calculaAllChurn( arquivo="../../ArquivosTransacoes/trans_clean.csv", freq="2W" )
+print(teste)
--- ChurnFinal/Frequencia/Recente/teste_churn.py
+++
@@ -1,6 +0,0 @@
-import Churn as c 
-
-e = c.e
-
-teste = c.calculaAllChurn( arquivo="../trans_clean.csv", freq="2W" )
-print(teste)
